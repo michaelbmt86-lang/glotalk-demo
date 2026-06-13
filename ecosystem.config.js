3acb7d3407fc650579970a036a273610d4decc3a
AI Lead Offers <michaelbmt86@gmail.com>
2026-06-13 20:22:25 +0800

    remove plaintext keys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,14 +2,6 @@ module.exports = {
   apps: [{
     name: 'glotalk-server',
     script: '/var/www/glotalk/server.js',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000,
-      ACCESS_TOKEN: 'glotalk2026',
-      ADMIN_PASS: 'gloAdmin2026',
-      GEMINI_API_KEY: 'AQ.Ab8RN6JU1ty-morix50yd0cwhohKPzEIUxF7c-rrHKDKOwqQPg',
-      OPENAI_API_KEY: '',
-      GITHUB_TOKEN: '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'
-    }
+    env_file: '/var/www/glotalk/.env'
   }]
 }
