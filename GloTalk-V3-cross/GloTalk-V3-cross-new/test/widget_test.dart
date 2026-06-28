9f94c3f99b144edf2317bcf6e4444a9933293da1
AI Lead Offers <michaelbmt86@gmail.com>
2026-06-29 06:40:19 +0800

    Remove GloTalk-V3-cross directory

--- GloTalk-V3-cross/GloTalk-V3-cross-new/test/widget_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:glotalk_v3_cross/main.dart';
-
-void main() {
-  testWidgets('GloTalk smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const GloTalkApp());
-  });
-}
